b867acdd48233549ad605de2612cdf5d130371fa
MatheusMorais506 <matheus.moraes09@hotmail.com>
2021-08-06 19:33:56 -0300

    new update

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 - JS
 - HTML
 - CSS
+
+# Código teclas do teclado
+https://docs.microsoft.com/pt-br/dotnet/api/system.windows.forms.keys?view=net-5.0
+
+# Sons
+https://freesound.org
--- script.js
+++ script.js
@@ -51,3 +51,21 @@ function registerEvents() {
 }
 
 window.addEventListener("load", registerEvents)
+
+//Data em Tempo Real
+// Função para formatar 1 em 01
+const zeroFill = n => {
+    return ('0' + n).slice(-2);
+}
+
+// Cria intervalo
+const interval = setInterval(() => {
+    // Pega o horário atual
+    const now = new Date();
+
+    // Formata a data conforme dd/mm/aaaa hh:ii:ss
+    const dataHora = zeroFill(now.getUTCDate()) + '/' + zeroFill((now.getMonth() + 1)) + '/' + now.getFullYear() + ' ' + zeroFill(now.getHours()) + ':' + zeroFill(now.getMinutes()) + ':' + zeroFill(now.getSeconds());
+
+    // Exibe na tela usando a div#data-hora
+    document.getElementById('visor').innerHTML = dataHora;
+}, 1000);
