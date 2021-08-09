efad6031a9d964aab7ef84727aac72ca6f8b311d
MatheusMorais506 <matheus.moraes09@hotmail.com>
2021-08-09 19:21:34 -0300

    update

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-// get all keys
 const keys = document.querySelectorAll(".key")
 
 function playNote(event) {
     let audioKeyCode = getKeyCode(event);
-    // typed or pressed key
     const key = document.querySelector(`.key[data-key="${audioKeyCode}"]`)
-        // if key exists
+
     if (!key) {
         return;
     }
-    addPlayingClass(key)
-    playAudio(audioKeyCode)
-}
+    if (event.shiftKey) {
 
-function addPlayingClass(key) {
-    key.classList.add('playing')
+    }
+    addPlayingClass(key) //adiciona clase css
+    playAudio(audioKeyCode) //Acionar Audio
+    visorNotas(key) //Adicionar notas no visor
 }
 
 function getKeyCode(event) {
     let keyCode;
-
     const isKeyboard = event.type === "keydown"
+
     if (isKeyboard) {
         keyCode = event.keyCode
     } else {
@@ -35,37 +33,88 @@ function playAudio(audioKeyCode) {
     audio.play()
 }
 
+function addPlayingClass(key) {
+    key.classList.add('playing')
+}
+
 function removePlayingClass(event) {
     event.target.classList.remove("playing")
 }
 
+const zeroFill = n => {
+    return ('0' + n).slice(-2);
+}
+const interval = setInterval(() => {
+    const now = new Date();
+    const dataHora = zeroFill(now.getUTCDate()) + '/' + zeroFill((now.getMonth() + 1)) + '/' + now.getFullYear() + ' ' + zeroFill(now.getHours()) + ':' + zeroFill(now.getMinutes()) + ':' + zeroFill(now.getSeconds());
+    document.getElementById('date').innerHTML = dataHora;
+}, 1000);
+
+function visorNotas(key) {
+
+    var notaMusical = document.getElementById('NotaMusic');
+    //const notas = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+
+    switch (key.id) {
+        case "C":
+            notaMusical.innerHTML = 'C'
+            console.log('C')
+            break;
+        case "C#":
+            notaMusical.innerHTML = 'C#'
+            console.log('C#')
+            break;
+        case "D":
+            notaMusical.innerHTML = 'D'
+            console.log('D')
+            break;
+        case "D#":
+            notaMusical.innerHTML = 'D#'
+            console.log('D#')
+            break;
+        case "E":
+            notaMusical.innerHTML = 'E'
+            console.log('E')
+            break;
+        case "F":
+            notaMusical.innerHTML = 'F'
+            console.log('F')
+            break;
+        case "F#":
+            notaMusical.innerHTML = 'F#'
+            console.log('F#')
+            break;
+        case "G":
+            notaMusical.innerHTML = 'G'
+            console.log('G')
+            break;
+        case "G#":
+            notaMusical.innerHTML = 'G#'
+            console.log('G#')
+            break;
+        case "A":
+            notaMusical.innerHTML = 'A'
+            console.log('A')
+            break;
+        case "A#":
+            notaMusical.innerHTML = 'A#'
+            console.log('A#')
+            break;
+        case "B":
+            notaMusical.innerHTML = 'B'
+            console.log('B')
+            break;
+        default:
+            notaMusical.innerHTML = '?'
+            console.log('?')
+    }
+}
+
 function registerEvents() {
-    // click with mouse
     keys.forEach(function(key) {
         key.addEventListener("click", playNote)
         key.addEventListener("transitionend", removePlayingClass)
     })
-
-    // keyboard type
     window.addEventListener("keydown", playNote)
 }
-
 window.addEventListener("load", registerEvents)
-
-//Data em Tempo Real
-// Função para formatar 1 em 01
-const zeroFill = n => {
-    return ('0' + n).slice(-2);
-}
-
-// Cria intervalo
-const interval = setInterval(() => {
-    // Pega o horário atual
-    const now = new Date();
-
-    // Formata a data conforme dd/mm/aaaa hh:ii:ss
-    const dataHora = zeroFill(now.getUTCDate()) + '/' + zeroFill((now.getMonth() + 1)) + '/' + now.getFullYear() + ' ' + zeroFill(now.getHours()) + ':' + zeroFill(now.getMinutes()) + ':' + zeroFill(now.getSeconds());
-
-    // Exibe na tela usando a div#data-hora
-    document.getElementById('visor').innerHTML = dataHora;
-}, 1000);
